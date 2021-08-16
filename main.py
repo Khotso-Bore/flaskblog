d88606591bd2d861e5eba0b55ca6d8ca175f074c
Khotso-Bore <u19180642@tuks.co.za>
2021-08-17 01:39:34 +0200

    added main

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "src")
+)
+
+from flask_blog import *  # noqa: E402 F403
+
+
+if __name__ == "__main__":
+    app = create_app()  # noqa: F405
+    app.run()
